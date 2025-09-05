d9913dbf43b89b3f8e88f7a12b3b925fbd25593e
XIAO-JUN-ZE <jeremy890808@gmail.com>
2025-09-05 18:08:59 +0800

    update client_config.py

--- client_config.py
+++ client_config.py
@@ -2,4 +2,5 @@ import os
 
 DOIP_SERVER_IP = os.environ.get("DOIP_SERVER_IP", "192.168.200.1") # Get an IP(FD) from the environment; use 192.168.200.1 if none is provided. 
 DoIP_LOGICAL_ADDRESS = int(os.environ.get("DoIP_LOGICAL_ADDRESS", "0x0680"), 0) # Get an DoIP_LOGICAL_ADDRESS(FD) from the environment; use 0x0680 if none is provided.
+DoIP_FUNCTION_ADDRESS = int(os.environ.get("DoIP_FUNCTION_ADDRESS", "0xEFFF"), 0)
 CLIENT_LOGICAL_ADDRESS = int(os.environ.get("CLIENT_LOGICAL_ADDRESS", "0x0E00"), 0) # Get an LIENT_LOGICAL_ADDRESS from the environment; use 0x0E00 if none is provided.
